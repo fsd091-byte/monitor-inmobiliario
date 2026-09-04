a9a590499bc6e9da40d64cbcaf866109d3f46fdd
fsd091-byte <fsd091@gmail.com>
2026-09-04 11:22:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import gestor_db
 import json
 import unicodedata
 from extractor import obtener_pisos_desde_db, obtener_pisos_idealista
+from extractor import obtener_pisos_desde_json, obtener_pisos_idealista
 
 # 1. Parámetros de filtrado
 PRECIO_MIN = 50000
